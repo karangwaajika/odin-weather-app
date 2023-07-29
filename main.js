e6216fd369c6f935fc2067bdaa8264c7bea4c071
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-29 16:51:34 +0200

    feat: display weather info

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const userInput = document.querySelector('.locationName');
 const searchBtn = document.querySelector('.btn');
 const image = document.querySelector('.image img');
+const infoDiv = document.querySelector('.info');
+const infoHeader = document.querySelector('.info-header');
+const infoPara1 = document.querySelector('.para1');
+const infoPara2 = document.querySelector('.para2');
 
 async function checkWeather(){
     const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${userInput.value}`;
@@ -13,12 +17,17 @@ async function checkWeather(){
         }
     };
     try {
+
         const response = await fetch(url, options);
         const result = await response.json();
         image.src = `https:${result.current.condition.icon}`;
-        console.log(image);
+        infoHeader.textContent = `${result.location.name}/${result.location.region}/${result.location.country}, ${result.current.condition.text}`;
+        infoPara1.textContent = `Feels like ${result.current.feelslike_c} degree celcius, humidity ${result.current.humidity}, cloud ${result.current.cloud}%, wind ${result.current.wind_mph} mph. `;
+        infoPara2.textContent = `Geo Coords [${result.location.lat}, ${result.location.lon}]`
+    
+        console.log(result);
     } catch (error) {
-        console.error(error);
+        console.error(`${error}`);
     }
 }
 
